447ebf06295fb7dfe28305ffc90c0950f1542b15
Genat <gh43836@ubt-uni.net>
2022-03-01 14:36:44 +0100

    inicimi

---
+++ Projekti/Departamenti.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekti
+{
+    internal class Departamenti
+    {
+        public string Emri { get; set; }
+        public string Email { get; set; }
+
+        public Departamenti(string emri, string email)
+        {
+            Emri=emri;
+            Email=email;
+        }
+
+
+    }
+}
