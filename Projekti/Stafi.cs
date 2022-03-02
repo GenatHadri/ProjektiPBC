2c60c22187631ed1c94cb4d5a853aa0c3cc6c0b7
Valmire <valmiregashi5@gmail.com>
2022-03-02 11:20:10 +0100

    Universiteti

--- Projekti/Stafi.cs
+++ Projekti/Stafi.cs
@@ -9,10 +9,21 @@ namespace Projekti
     internal class Stafi
     {
         private int Id { get; set; }
+        public int _Id { get { return Id; } }
         public string Emri { get; set; }
         public string Mbiemri { get; set; }
         public string Email { get; set; }
         public string nrTel { get; set; }
         public string Angazhimi { get; set; }
+
+        public Stafi(int id, string emri, string mbiemri, string email, string nrTel, string angazhimi)
+        {
+            Id = id;
+            Emri = emri;
+            Mbiemri = mbiemri;
+            Email = email;
+            this.nrTel = nrTel;
+            Angazhimi = angazhimi;
+        }
     }
 }
--- Projekti/Universiteti.cs
+++ Projekti/Universiteti.cs
@@ -8,5 +8,24 @@ namespace Projekti
 {
     internal class Universiteti
     {
+        private int Id { get { return Id; } }
+        private string EmriUniversitetit { get; set; }
+        public string Email { get; set; }
+        Departamenti departamenti { get; set; }
+
+        public Universiteti(Departamenti departamenti)
+        {
+            this.departamenti = departamenti;
+        }
+
+        public Universiteti(string emriUniversitetit, string email, Departamenti departamenti)
+        {
+         
+            EmriUniversitetit = emriUniversitetit;
+            Email = email;
+            this.departamenti = departamenti;
+
+
+        }
     }
 }
