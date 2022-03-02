d2d95ae5656f8b498c66ee9b2f6e1c01f798514b
HP <HP@DESKTOP-TNTR5VA>
2022-03-02 11:43:59 +0100

    jon vendos exception te personi

--- Projekti/Alban/Personi.cs
+++ Projekti/Alban/Personi.cs
@@ -10,11 +10,25 @@ namespace Projekti
     {
         private string Emri { get; set; }
         private string Mbiemri { get; set; }
+        private string Email { get; set; }
 
-        protected Personi(string emri, string mbiemri)
+        protected Personi(string emri, string mbiemri, string email)
         {
+            if(emri == null || emri.Trim() == "")
+            {
+                throw new ProjektiException("Emri eshte null!");
+            }
+            if(mbiemri == null || mbiemri.Trim() == "")
+            {
+                throw new ProjektiException("Email eshte null!");
+            }
+            if(email == null || email.Trim() == "")
+            {
+                throw new ProjektiException("Email eshte null!");
+            }
             Emri = emri;
             Mbiemri = mbiemri;
+            Email = email;
         }
 
     }
