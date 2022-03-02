d30a0194fc1a142a98523f15cf7471b5163771e6
HP <HP@DESKTOP-TNTR5VA>
2022-03-02 11:17:27 +0100

    Namespace e ka pas emrin gabim. Nven "Projekti" e ka pas "TemaDiplomes" -Alban

--- Projekti/Alban/Profesori.cs
+++ Projekti/Alban/Profesori.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TemaDiplomes.NewFolder
+namespace Projekti
 {
     public class Profesori : StafiAkademik
     {
--- Projekti/Komisioni.cs
+++ Projekti/Komisioni.cs
@@ -8,7 +8,9 @@ namespace Projekti
 {
     class Komisioni
     {
-        Profesori profesori { get; set; }
+        Profesori p1 { get; set; }
+        Profesori p2 { get; set; }
+        Profesori p3 { get; set; }
         public Komisioni(Profesori p1, Profesori p2, Profesori p3)
         {
             if(p1 == p2 || p2 == p3 || p3 == p1)
