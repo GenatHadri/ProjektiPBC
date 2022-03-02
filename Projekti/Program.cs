df970df68f38f2c31836d3683d4a270751dd15fe
Genat <gh43836@ubt-uni.net>
2022-03-02 10:22:05 +0100

    Studenti dhe lenda

--- Projekti/Program.cs
+++ Projekti/Program.cs
@@ -10,7 +10,13 @@ namespace Projekti
     {
         static void Main(string[] args)
         {
+            Lenda l1 = new Lenda("183209", "Shkenca Kompjuterike1", 5);
+            Lenda l2 = new Lenda("234034", "Web Technologies and Services", 5);
+            Studenti s1 = new Studenti(181943836, "Genat", "Hadri", "gh43836@ubt-uni.net", "Shki", false, new Lenda[] { l1, l2});
 
+            Console.WriteLine(l1);
+            Console.WriteLine(l2);
+            Console.WriteLine(s1);
         }
     }
 }
