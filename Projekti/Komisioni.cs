4872461cc7743f6f0329350d37626012e34b60c7
HP <HP@DESKTOP-TNTR5VA>
2022-03-02 11:07:38 +0100

    komisioni

--- Projekti/Komisioni.cs
+++ Projekti/Komisioni.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TemaDiplomes.NewFolder;
 
 namespace Projekti
 {
     class Komisioni
     {
-
+        Profesori profesori { get; set; }
+        public Komisioni(Profesori p1, Profesori p2, Profesori p3)
+        {
+            if(p1 == p2 || p2 == p3 || p3 == p1)
+            {
+                throw new ProjektiException("Nuk mund te jene 2 ose 3 profesora te njejte ne komision!");
+            }
+        }
     }
 }
