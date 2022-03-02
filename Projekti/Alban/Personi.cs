7cdb6c57b75a48eb6e901bd753453d54092ee429
Alban <albanselmanaj1999@gmail.com>
2022-03-02 10:50:05 +0100

    update on folder

---
+++ Projekti/Alban/Personi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaDiplomes.NewFolder
+{
+    public class Personi
+    {
+        protected string Emri;
+        protected string Mbiemri;
+        public string _Emri { get { return Emri; } set { Emri = value; } }
+        public string _Mbiemri { get { return Mbiemri; } set { Mbiemri = value; } }
+    }
+}
---
+++ Projekti/Alban/Profesori.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaDiplomes.NewFolder
+{
+    public class Profesori : StafiAkademik
+    {
+        private string Grada;
+
+        public string _Grada { get { return Grada; } set { Grada = value; } }
+
+
+        public Profesori(string Grada,string Emri, string Mbiemri, int ID, string Email, string NrTel, int Angazhimi) : base(Emri, Mbiemri, ID, Email, NrTel)
+        {
+            this.Grada = Grada;
+
+        }
+        public  string ToString()
+        {
+            return $"{ this.Grada}.{ this.Emri} { this.Mbiemri} Email:{this.Email}";
+        }
+    }
+}
