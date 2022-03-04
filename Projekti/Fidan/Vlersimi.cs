8baf9944d9a71c357025dd0153a7fec9af8e0a21
rilind bejta <rini_bejta@hotmail.com>
2022-03-04 14:28:32 +0100

    tema update

--- Projekti/Fidan/Vlersimi.cs
+++ Projekti/Fidan/Vlersimi.cs
@@ -10,7 +10,6 @@ namespace Projekti.Fidan
     internal class Vlersimi
     {
         private int Nota;
-        private Tema tema;
         private Komisioni komisioni;
         private Kerkesa kerkesa;
         public Vlersimi(Komisioni komisioni, Kerkesa kerkesa, int nota)
@@ -19,12 +18,10 @@ namespace Projekti.Fidan
             {
                 throw new ProjektiException("nota eshte shenuar gabim");
             }
-            this.tema = tema;
             this.Nota1 = Nota1;
         }
        
         public int Nota1 { get => Nota; set => Nota = value; }
-        internal Tema Tema { get => tema; set => tema = value; }
         internal Komisioni Komisioni { get => komisioni; set => komisioni = value; }
         internal Kerkesa Kerkesa { get => kerkesa; set => kerkesa = value; }
 
